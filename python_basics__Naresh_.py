37e988ba96cae3ceb3353217300b30d27fa4ea8d
nareshbadavath2000-web <nareshbadavath2000@gmail.com>
2026-04-15 16:49:59 +0530

    Add files via upload

---
+++ python_basics__Naresh_.py
@@ -0,0 +1,21 @@
+# Task 1: Variables and Data Types
+name = "Naresh"
+age = 26
+height = 1.75
+is_student = True
+
+print(name, age, height, is_student)
+
+# Task 2: String Formatting
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+# Task 3: Arithmetic Operations
+age_in_months = age * 26
+age_in_days = age * 365
+remainder = age % 7
+age_squared = age ** 2
+
+print("Age in months:", age_in_months)
+print("Age in days:", age_in_days)
+print("Remainder when age is divided by 7:", remainder)
+print("Age squared:", age_squared)
